8bcda0821a0e9d0c572917f44231a9d9b68845ca
Terance Edmonds <teranceedmonds20@gmail.com>
2024-06-11 17:12:01 +0530

    initial commit

---
+++ src/main/java/com/holomentor/holomentor/HolomentorApplication.java
@@ -0,0 +1,13 @@
+package com.holomentor.holomentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolomentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolomentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holomentor/holomentor/HolomentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holomentor.holomentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolomentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
