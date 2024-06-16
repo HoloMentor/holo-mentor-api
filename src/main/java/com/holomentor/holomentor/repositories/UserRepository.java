a1f44eaaaa4990ad59b9469280972b99caedd704
Terance Edmonds <teranceedmonds20@gmail.com>
2024-06-16 20:20:35 +0530

    user login

--- src/main/java/com/holomentor/holomentor/repositories/UserRepository.java
+++ src/main/java/com/holomentor/holomentor/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.holomentor.holomentor.repositories;
 
-public class UserRepository {
-}
+import com.holomentor.holomentor.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
--- src/main/java/com/holomentor/holomentor/resources/UserResource.java
+++ src/main/java/com/holomentor/holomentor/resources/UserResource.java
@@ -1,4 +1,26 @@
 package com.holomentor.holomentor.resources;
 
+import com.holomentor.holomentor.models.User;
+import com.holomentor.holomentor.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("users")
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @GetMapping("/get")
+    public String get() {
+        return "user";
+    }
 }
