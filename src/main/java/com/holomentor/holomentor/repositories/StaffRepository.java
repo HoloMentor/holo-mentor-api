1747edcefc0a9d37c647e1b59054e322d827c1a9
yashed <yashedthisara2001@gmail.com>
2024-09-09 16:36:53 +0530

    rename anudas changers

--- src/main/java/com/holomentor/holomentor/repositories/StaffRepository.java
+++ src/main/java/com/holomentor/holomentor/repositories/StaffRepository.java
@@ -18,16 +18,4 @@ import java.util.Optional;
 @Transactional
 public interface StaffRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findById(Long id);
-    Page<InstituteSubject> findByNameContainingIgnoreCaseAndInstituteId(String name, Long instituteId, Pageable pageable);
-
-
-    @Query("SELECT s.id as id, s.name as name, s.instituteId as instituteId, s.createdAt as createdAt, COUNT(DISTINCT c.id) AS classCount, COUNT(DISTINCT cs.studentId) AS studentCount " +
-            "FROM InstituteSubject s " +
-            "LEFT JOIN InstituteClass c ON s.id = c.subjectId " +
-            "LEFT JOIN InstituteClassStudent cs ON c.id = cs.classId " +
-            "WHERE s.name ILIKE %:name% AND s.instituteId = :instituteId " +
-            "GROUP BY s.id, s.name")
-    Page<InstituteClassProjection> findByClassByInstitute(String name, Long instituteId, Pageable pageable);
-
 }
