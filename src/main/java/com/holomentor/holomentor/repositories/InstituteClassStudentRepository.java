aac448e0707b0c4850890bc0ce6626f744e73faa
yashed <yashedthisara2001@gmail.com>
2024-11-25 19:42:10 +0530

    pull main

--- src/main/java/com/holomentor/holomentor/repositories/InstituteClassStudentRepository.java
+++ src/main/java/com/holomentor/holomentor/repositories/InstituteClassStudentRepository.java
@@ -1,13 +1,21 @@
 package com.holomentor.holomentor.repositories;
 
+import com.holomentor.holomentor.models.InstituteClassStudent;
 import com.holomentor.holomentor.models.User;
 import com.holomentor.holomentor.projections.instituteClass.InstituteClassStudentProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.Query;
 
-public interface InstituteClassStudentRepository extends JpaRepository<User, Long> {
+import java.util.List;
+
+@Repository
+@Transactional
+public interface InstituteClassStudentRepository extends JpaRepository<InstituteClassStudent, Long> {
+    List<InstituteClassStudent> findByClassId(Long classId);
 
     @Query("select u.id as id , u.firstName as firstName, u.lastName as lastName "+
             "FROM User u "+
